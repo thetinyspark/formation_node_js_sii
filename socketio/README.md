cf1499de8b87687495a87c66de5b81daa3fdb204
thetinyspark <contact@the-tiny-spark.com>
2022-02-08 17:01:31 +0100

    add underscore

---
+++ socketio/README.md
@@ -0,0 +1,9 @@
+# Exercice websocket 
+
+- Envoyer une suite de message à l'aide d'un évènement customisé
+de type channel:msg. 
+- Analyser les données envoyées afin de séparer le pseudo de l'utilisateur 
+du message qu'il envoie. 
+- Conseil: Faire en sorte que le message soit sous la forme <pseudo>:<msg>
+- Créer une commande permettant de retourner tous les messages d'un pseudo en particulier 
+(utiliser underscore);
--- socketio/server/index.js
+++ socketio/server/index.js
@@ -1,10 +1,61 @@
 const socketio  = require("socket.io");
 const server    = socketio(3000);
+const _         = require("underscore");
+
+const serverEvents = [
+    {
+        type:"request:connection", 
+        requestParams:[], 
+        clientSide: true
+    }, 
+    {
+        clientSide: true,
+        type:"channel:push:msg", 
+        requestParams:[
+            {
+                name:"channelId", 
+                type: "number"
+            }, 
+            {
+                name:"message", 
+                type:"string"
+            }
+        ]
+    }, 
+    {
+        clientSide: false, 
+        type: "internal", 
+        requestParams:[]
+    }
+]
 
 function welcomeHandler(clientSocket){
+    const list = _.map( 
+        _.filter(
+            serverEvents, 
+            {clientSide: true}
+        ), 
+        (currentServerEvent)=>{
+            const paramList = _.map( 
+                currentServerEvent.requestParams, 
+                (param)=>{
+                    return `${param.name}<${param.type}>,`;
+                }
+            );
+
+            return `
+                ${currentServerEvent.type} - [${paramList.join("")}]
+            `;
+        }
+    );
+
     clientSocket.emit(
         "connection:accepted",
-        "Welcome on this server, please be respectful"
+        `
+        ******* Welcome on this server, please be respectful ****** 
+        List of events you can send: 
+        ${list.join("")}
+        `
     );
 }
 
