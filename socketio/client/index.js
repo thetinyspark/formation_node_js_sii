46a3670d20f64c93257b25dcc8985500782163a4
thetinyspark <contact@the-tiny-spark.com>
2022-02-08 17:16:50 +0100

    add correction

--- socketio/client/index.js
+++ socketio/client/index.js
@@ -3,6 +3,17 @@ const socket = io("ws://localhost:3000");
 
 function onConnectionAccepted(msg){
     console.log(msg);
+
+    socket.emit("channel:msg", "bilbon: Je suis vieux Gandalf");
+    socket.emit("channel:msg", "gandalf: Flamme doudoune");
+    socket.emit("channel:msg", "gandalf: vous ne passerez pas");
+    socket.emit("channel:msg", "gollum: mon précieux");
+
+    socket.emit("request:pseudo:msg", "gandalf");
+}
+
+function onReceivePseudoMessage(msg){
+    console.log(msg);
 }
 
 function start(){
@@ -11,7 +22,6 @@ function start(){
     socket.emit("request:connection","");
 }
 socket.on("connect", start);
-socket.on(
-    "connection:accepted", 
-    onConnectionAccepted
-);
+socket.on("connection:accepted", onConnectionAccepted);
+socket.on("receive:pseudo:msg", onReceivePseudoMessage);
+
--- socketio/server/index.js
+++ socketio/server/index.js
@@ -2,68 +2,54 @@ const socketio  = require("socket.io");
 const server    = socketio(3000);
 const _         = require("underscore");
 
-const serverEvents = [
-    {
-        type:"request:connection", 
-        requestParams:[], 
-        clientSide: true
-    }, 
-    {
-        clientSide: true,
-        type:"channel:push:msg", 
-        requestParams:[
-            {
-                name:"channelId", 
-                type: "number"
-            }, 
-            {
-                name:"message", 
-                type:"string"
-            }
-        ]
-    }, 
-    {
-        clientSide: false, 
-        type: "internal", 
-        requestParams:[]
-    }
-]
+const channelMessages = [];
 
 function welcomeHandler(clientSocket){
-    const list = _.map( 
-        _.filter(
-            serverEvents, 
-            {clientSide: true}
-        ), 
-        (currentServerEvent)=>{
-            const paramList = _.map( 
-                currentServerEvent.requestParams, 
-                (param)=>{
-                    return `${param.name}<${param.type}>,`;
-                }
-            );
-
-            return `
-                ${currentServerEvent.type} - [${paramList.join("")}]
-            `;
-        }
-    );
 
     clientSocket.emit(
         "connection:accepted",
         `
         ******* Welcome on this server, please be respectful ****** 
-        List of events you can send: 
-        ${list.join("")}
         `
     );
 }
 
+function onChannelMessage(clientSocket, msg){
+    const parts = msg.split(":");
+    const pseudo = parts[0]; 
+    const realMsg = parts[1];
+    channelMessages.push(
+        {
+            pseudo, 
+            msg: realMsg
+        }
+    );
+}
+
+function onRequestPseudoMessage(clientSocket, msg){
+    const pseudo = msg;
+    const messages = _.filter(channelMessages, {pseudo: pseudo}); 
+    clientSocket.emit("receive:pseudo:msg", messages);
+}
+
 function init(clientSocket){
     clientSocket.on(
-        "request:connection", ()=>{
+        "request:connection", 
+        ()=>{
             welcomeHandler(clientSocket);
         }
     );
+    clientSocket.on( 
+        "channel:msg", 
+        (msg)=>{
+            onChannelMessage(clientSocket, msg)
+        }
+    );
+    clientSocket.on(
+        "request:pseudo:msg", 
+        (msg)=>{
+            onRequestPseudoMessage(clientSocket, msg);
+        }
+    )
 }
 server.on("connection", init);
